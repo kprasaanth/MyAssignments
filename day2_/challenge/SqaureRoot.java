a5493c845506800f42f4f3152ed82b389e87d1ab
kprasaanth <119736622+kprasaanth@users.noreply.github.com>
2022-12-07 21:55:14 +0530

    Add files via upload
    Day 2 Challenge code is uploaded.

---
+++ day2_/challenge/SqaureRoot.java
@@ -0,0 +1,14 @@
+package day2_.challenge;
+
+public class SqaureRoot {
+
+	public static void main(String[] args) {
+		int num = 17;
+		
+		int a= (int) Math.sqrt(num);
+		
+		System.out.println(a);
+
+	}
+
+}
